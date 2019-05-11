eb859603369803d34c81aef2a02be4d4a4a5faf7
sbm367 <sbm367@protonmail.com>
2019-05-10 20:54:49 -0400

    added flask

--- Eden.py
+++ Eden.py
@@ -1,7 +1,18 @@
+'''
 import os
 import pandas as pd
 import nltk
+'''
+from flask import Flask
 
-print("init commit")
+app = Flask(__name__)
 
-nltk.download()
+
+@app.route('/')
+def hello():
+    return '<h1>Deployed to Heroku!</h1>'
+	
+	
+#print("this is only a test uwu")
+
+#nltk.download()
